1095b0b6be5870cf84d9a6f568af40cc6cc792fa
Vinu Suhas <vinusuhas2000@gmail.com>
2023-09-05 00:26:17 +0530

    bugs : image and auto img set fixed

--- scripts.js
+++ scripts.js
@@ -19,7 +19,41 @@ function current_date(){
     secs.innerText=seconds
 }
 
+function auto_settime(){
+    let dateobj=new Date()
+    let hours=dateobj.getHours()
+    let night_id=document.getElementById('night_menu')
+    let nap_id=document.getElementById('nap_menu')
+    let lunch_id=document.getElementById('lunch_menu')
+    let wake_up_id=document.getElementById('wake_up_menu')
+    let night_time_viewer=document.getElementById('night_time_display')
+    let nap_time_viewer=document.getElementById('nap_time_display')
+    let lunch_time_viewer=document.getElementById('lunch_time_display')
+    let wake_up_time_viewer=document.getElementById('wake_up_time_display')
 
+    if(hours >=11){
+        // morning 
+        document.getElementById('box3_greeting').innerText="GOOD MORNING!! WAKE UP !! "
+        document.getElementById('image').src="./morning_greeting.svg"
+        document.getElementById('box7_greeting').innerText="GRAB SOME HEALTHY BREAKFAST!!! "
+    }
+    else if(hours >=13){
+        document.getElementById('box3_greeting').innerText="GOOD AFTERNOON TAKE SOME SLEEP"
+        document.getElementById('image').src="./lunch_greeting.png"
+        document.getElementById('box7_greeting').innerText="LET'S HAVE SOME LUNCH !!"
+    }
+    else if(hours >=17){
+        document.getElementById('box3_greeting').innerText="GOOD EVENING !!"
+        document.getElementById('image').src="./good_evening.png"
+        document.getElementById('box7_greeting').innerText='STOP YAWNING, GET SOME TEA.. \n ITS JUST EVENING!'
+    }
+    else {
+        document.getElementById('box3_greeting').innerText="GOOD NIGHT !!"
+        document.getElementById('image').src="./good_night.png"
+        document.getElementById('box7_greeting').innerText='CLOSE YOUR EYES AND GO TO SLEEP'
+    }
+}
+auto_settime()
 function set_alarm_button(){
     let dateobj=new Date()
     let hours=dateobj.getHours()
@@ -47,7 +81,7 @@ function set_alarm_button(){
     }
     else if(nap_id.value == hours){
         document.getElementById('box3_greeting').innerText="GOOD EVENING !!"
-        document.getElementById('image').src="./nap_greeting.svg"
+        document.getElementById('image').src="./good_evening.png"
         document.getElementById('box7_greeting').innerText='STOP YAWNING, GET SOME TEA.. \n ITS JUST EVENING!'
     }
     else if(lunch_id.value ==hours){
